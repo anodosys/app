c9978736946fe98058726420e4569ba8e6a429dc
Andreas Knollmann <aknollmann@tofex.de>
2022-08-22 19:50:14 +0200

    fix image used

--- lib/image/used.sh
+++ lib/image/used.sh
@@ -7,16 +7,25 @@ imageUsed()
   local imageId
   local nextImageId
   local historyImageId
+
   if [[ $(imageExists "${imageName}" "${imageTag}") == 1 ]]; then
     imageId=$(docker image inspect --format '{{.Id}}' "${imageName}:${imageTag}")
     # shellcheck disable=SC2046
-    if [[ -n "${imageId}" ]] && [[ $(docker container inspect $(docker container ls -aq) --format "{{ if eq .Image \"${imageId}\" }}{{.Id}}{{end}}" | grep -v '^$' | wc -l) -gt 0 ]]; then
-      echo "1"
-      exit 0
+    if [[ -n "${imageId}" ]]; then
+      containerIds=( $(docker container ls -aq) )
+      if [[ "${#containerIds[@]}" -gt 0 ]]; then
+        if [[ $(docker container inspect "${containerIds[@]}" --format "{{ if eq .Image \"${imageId}\" }}{{.Id}}{{end}}" | grep -v '^$' | wc -l) -gt 0 ]]; then
+          echo "1"
+          exit 0
+        fi
+      fi
     else
-      if [[ $(for nextImageId in $(docker image ls -a | tail -n +2 | awk '{print $3}'); do if [[ "${nextImageId}" != "${imageId}" ]]; then for historyImageId in $(docker image history "${nextImageId}" | awk '{print $1}'); do if [[ "${historyImageId}" == "${imageId}" ]]; then echo "${nextImageId}"; fi; done; fi; done | wc -l) -gt 0 ]]; then
-        echo "1"
-        exit 0
+      imageIds=( $(docker image ls -a | tail -n +2 | awk '{print $3}') )
+      if [[ "${#imageIds[@]}" -gt 0 ]]; then
+        if [[ $(for nextImageId in "${imageIds[@]}"; do if [[ "${nextImageId}" != "${imageId}" ]]; then for historyImageId in $(docker image history "${nextImageId}" | awk '{print $1}'); do if [[ "${historyImageId}" == "${imageId}" ]]; then echo "${nextImageId}"; fi; done; fi; done | wc -l) -gt 0 ]]; then
+          echo "1"
+          exit 0
+        fi
       fi
     fi
   fi
