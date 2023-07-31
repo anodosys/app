86e8e777e773f11bfaddc8f60b8be36d7aa13b89
Andreas Knollmann <a.knollmann@seak.dev>
2023-07-31 12:45:51 +0200

    update parameter handling to allow skipping

--- app/prepare-parameters.sh
+++ app/prepare-parameters.sh
@@ -7,7 +7,7 @@ unparsedParameters=( )
 while [[ "$#" -gt 0 ]]; do
   parameter="${1}"
   shift
-  if [[ "${parameter:0:2}" == "--" ]] || [[ "${parameter}" =~ ^-[[:alpha:]][[:space:]]+ ]] || [[ "${parameter}" =~ ^-\?$ ]]; then
+  if [[ "${parameter}" =~ ^-[[:alpha:]]+ ]] || [[ "${parameter}" =~ ^--[[:alpha:]]+ ]] || [[ "${parameter}" =~ ^-[[:alpha:]][[:space:]]+ ]] || [[ "${parameter}" =~ ^-\?$ ]]; then
     if [[ "${parameter}" =~ ^--[[:alpha:]]+[[:space:]]+ ]]; then
       parameter="${parameter:2}"
       prepareParametersKey=$(echo "${parameter}" | grep -oP '[[:alpha:]]+(?=\s)' | tr -d "\n")
@@ -21,29 +21,35 @@ while [[ "$#" -gt 0 ]]; do
       prepareParametersListParts+=( "${prepareParametersValue}" )
       continue
     fi
+    skipParameter=0
     if [[ "${parameter:0:2}" == "--" ]]; then
       prepareParametersKey="${parameter:2}"
     elif [[ "${parameter}" =~ ^-\?$ ]]; then
       prepareParametersKey="help"
     else
       prepareParametersKey="${parameter:1}"
+      skipParameter=1
     fi
     if [[ "$#" -eq 0 ]]; then
-      prepareParameters["${prepareParametersKey}"]=1
-      eval "${prepareParametersKey}=1"
-      #echo eval "${prepareParametersKey}=1"
-      prepareParametersList+=( "--${prepareParametersKey}" )
-      prepareParametersListParts+=( "--${prepareParametersKey}" )
-      prepareParametersListParts+=( "1" )
-    else
-      prepareParametersValue="${1}"
-      if [[ "${prepareParametersValue:0:2}" == "--" ]]; then
+      if [[ "${skipParameter}" == 0 ]]; then
         prepareParameters["${prepareParametersKey}"]=1
         eval "${prepareParametersKey}=1"
         #echo eval "${prepareParametersKey}=1"
         prepareParametersList+=( "--${prepareParametersKey}" )
         prepareParametersListParts+=( "--${prepareParametersKey}" )
         prepareParametersListParts+=( "1" )
+      fi
+    else
+      prepareParametersValue="${1}"
+      if [[ "${prepareParametersValue}" =~ ^-[[:alpha:]]+ ]] || [[ "${prepareParametersValue}" =~ ^--[[:alpha:]]+ ]]; then
+        if [[ "${skipParameter}" == 0 ]]; then
+          prepareParameters["${prepareParametersKey}"]=1
+          eval "${prepareParametersKey}=1"
+          #echo eval "${prepareParametersKey}=1"
+          prepareParametersList+=( "--${prepareParametersKey}" )
+          prepareParametersListParts+=( "--${prepareParametersKey}" )
+          prepareParametersListParts+=( "1" )
+        fi
         continue
       fi
       shift
