da66e2854c489a11eed3feba5e45bfaefa61a441
Andreas Knollmann <a.knollmann@seak.dev>
2023-08-16 23:59:19 +0200

    added local image source is source images

--- app/server/image/pull-source.sh
+++ app/server/image/pull-source.sh
@@ -47,7 +47,11 @@ logName "${systemName}" "${serverName}"
 setServerConfiguration "${systemName}" "${serverName}"
 
 if [[ -z "${skipImageCheck}" ]]; then
-  skipImageCheck="false"
+  if [[ -n "${local}" ]] && [[ "${local}" == 1 ]]; then
+    skipImageCheck="true"
+  else
+    skipImageCheck="false"
+  fi
 fi
 
 if [[ -n "${beforeImagePullSourceScript}" ]]; then
