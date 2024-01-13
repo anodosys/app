0443244503f75da9e24151508d6430eb3fc589e9
Andreas Knollmann <a.knollmann@seak.dev>
2024-01-13 20:35:47 +0100

    added check if path exists

--- lib/container/path.sh
+++ lib/container/path.sh
@@ -18,135 +18,139 @@ containerPath()
 
   if [[ $(containerExists "${containerName}") == 1 ]]; then
     if [[ -n "${containerPath}" ]]; then
-      if [[ -n "${accessRights}" ]] && [[ "${accessRights}" != "-" ]]; then
-        echo "Setting access rights ${accessRights} to container path: ${containerPath}"
-        containerCommand "${containerName}" "[[ -e ${containerPath} ]] && chmod ${accessRights} ${containerPath} || mkdir -p ${containerPath} && chmod ${accessRights} ${containerPath}"
-      fi
-
-      containerGroupId=$(containerCommandQuiet "${containerName}" "stat -c \"%g\" ${containerPath}")
-      containerGroupName=$(containerCommandQuiet "${containerName}" "getent group ${containerGroupId} | tr ':' ' ' | awk '{print \$1}'")
-      containerGroupName=$(prepareValue "${containerGroupName}")
-
-      if [[ -z "${containerGroupName}" ]]; then
-        containerGroupName="docker_volume_${containerGroupId}"
-        echo "Creating new group: ${containerGroupName}"
-        containerCommand "${containerName}" "groupadd -g ${containerGroupId} ${containerGroupName}"
-      else
-        echo "No need to create group: ${containerGroupName}"
-      fi
-
-      containerUserId=$(containerCommandQuiet "${containerName}" "stat -L -c \"%u\" ${containerPath}")
-      containerUserName=$(containerCommandQuiet "${containerName}" "getent passwd ${containerUserId} | cat" | tr ':' ' ' | awk '{print $1}')
-      containerUserName=$(prepareValue "${containerUserName}")
-
-      if [[ -z "${containerUserName}" ]]; then
-        containerUserName="docker_volume_${containerUserId}"
-        echo "Creating new user: ${containerUserName}"
-        containerCommand "${containerName}" "useradd -m -u ${containerUserId} -g ${containerGroupId} ${containerUserName}"
-      else
-        echo "No need to create user: ${containerUserName}"
-      fi
-
-      if [[ "${containerUserName}" != "${accessUser}" ]]; then
-        result=$(containerCommandQuiet "${containerName}" "id -nG ${accessUser} | grep -w ${containerGroupName} | wc -l")
-        result=$(prepareValue "${result}")
-
-        if [[ "${result}" == 0 ]]; then
-          echo "Adding user: ${accessUser} to group: ${containerGroupName}"
-          containerCommand "${containerName}" "usermod -a -G ${containerGroupName} ${accessUser}"
-        else
-          echo "No need to add user: ${accessUser} to group: ${containerGroupName}"
+      if [[ $(containerCommandQuiet "${containerName}" "[[ -e ${containerPath} ]] && echo \"true\" || echo \"false\"") == "true" ]]; then
+        if [[ -n "${accessRights}" ]] && [[ "${accessRights}" != "-" ]]; then
+          echo "Setting access rights ${accessRights} to container path: ${containerPath}"
+          containerCommand "${containerName}" "[[ -e ${containerPath} ]] && chmod ${accessRights} ${containerPath} || echo \"Path not available\""
         fi
-      else
-        echo "No different user for container path: ${containerPath}"
-      fi
 
-      if [[ "${containerUserName}" == "${accessUser}" ]]; then
-        accessPrefix="u"
-        accessName="user"
-      else
-        accessPrefix="g"
-        accessName="group"
-      fi
+        containerGroupId=$(containerCommandQuiet "${containerName}" "stat -c \"%g\" ${containerPath}")
+        containerGroupName=$(containerCommandQuiet "${containerName}" "getent group ${containerGroupId} | tr ':' ' ' | awk '{print \$1}'")
+        containerGroupName=$(prepareValue "${containerGroupName}")
 
-      if [[ "${mode}" == "r" ]] || [[ "${mode}" == "rr" ]]; then
-        if [[ "${mode}" == "r" ]]; then
-          echo "Adding ${accessName} read access rights to container path: ${containerPath}"
-          containerCommand "${containerName}" "chmod ${accessPrefix}+r ${containerPath}"
+        if [[ -z "${containerGroupName}" ]]; then
+          containerGroupName="docker_volume_${containerGroupId}"
+          echo "Creating new group: ${containerGroupName}"
+          containerCommand "${containerName}" "groupadd -g ${containerGroupId} ${containerGroupName}"
         else
-          echo "Adding ${accessName} read access rights to container path: ${containerPath} recursively"
-          containerCommand "${containerName}" "chmod -R ${accessPrefix}+r ${containerPath}"
+          echo "No need to create group: ${containerGroupName}"
         fi
-        accessRights=$(containerCommandQuiet "${containerName}" "stat -L -c \"%a\" ${containerPath}")
+
+        containerUserId=$(containerCommandQuiet "${containerName}" "stat -L -c \"%u\" ${containerPath}")
+        containerUserName=$(containerCommandQuiet "${containerName}" "getent passwd ${containerUserId} | cat" | tr ':' ' ' | awk '{print $1}')
+        containerUserName=$(prepareValue "${containerUserName}")
+
+        if [[ -z "${containerUserName}" ]]; then
+          containerUserName="docker_volume_${containerUserId}"
+          echo "Creating new user: ${containerUserName}"
+          containerCommand "${containerName}" "useradd -m -u ${containerUserId} -g ${containerGroupId} ${containerUserName}"
+        else
+          echo "No need to create user: ${containerUserName}"
+        fi
+
+        if [[ "${containerUserName}" != "${accessUser}" ]]; then
+          result=$(containerCommandQuiet "${containerName}" "id -nG ${accessUser} | grep -w ${containerGroupName} | wc -l")
+          result=$(prepareValue "${result}")
+
+          if [[ "${result}" == 0 ]]; then
+            echo "Adding user: ${accessUser} to group: ${containerGroupName}"
+            containerCommand "${containerName}" "usermod -a -G ${containerGroupName} ${accessUser}"
+          else
+            echo "No need to add user: ${accessUser} to group: ${containerGroupName}"
+          fi
+        else
+          echo "No different user for container path: ${containerPath}"
+        fi
+
         if [[ "${containerUserName}" == "${accessUser}" ]]; then
-          accessRight="${accessRights:0:1}"
+          accessPrefix="u"
+          accessName="user"
         else
-          accessRight="${accessRights:1:1}"
+          accessPrefix="g"
+          accessName="group"
         fi
-        if [[ "${accessRight}" == 4 ]] || [[ "${accessRight}" == 5 ]] || [[ "${accessRight}" == 6 ]] || [[ "${accessRight}" == 7 ]]; then
-          echo "Successfully added ${accessName} read access rights to container path: ${containerPath}" | sed $'s,.*,\e[1;36m&\e[m,'
-        else
-          >&2 echo "Could not add ${accessName} read access rights to container path: ${containerPath}"
+
+        if [[ "${mode}" == "r" ]] || [[ "${mode}" == "rr" ]]; then
+          if [[ "${mode}" == "r" ]]; then
+            echo "Adding ${accessName} read access rights to container path: ${containerPath}"
+            containerCommand "${containerName}" "chmod ${accessPrefix}+r ${containerPath}"
+          else
+            echo "Adding ${accessName} read access rights to container path: ${containerPath} recursively"
+            containerCommand "${containerName}" "chmod -R ${accessPrefix}+r ${containerPath}"
+          fi
+          accessRights=$(containerCommandQuiet "${containerName}" "stat -L -c \"%a\" ${containerPath}")
+          if [[ "${containerUserName}" == "${accessUser}" ]]; then
+            accessRight="${accessRights:0:1}"
+          else
+            accessRight="${accessRights:1:1}"
+          fi
+          if [[ "${accessRight}" == 4 ]] || [[ "${accessRight}" == 5 ]] || [[ "${accessRight}" == 6 ]] || [[ "${accessRight}" == 7 ]]; then
+            echo "Successfully added ${accessName} read access rights to container path: ${containerPath}" | sed $'s,.*,\e[1;36m&\e[m,'
+          else
+            >&2 echo "Could not add ${accessName} read access rights to container path: ${containerPath}"
+            exit 1
+          fi
+        elif [[ "${mode}" == "w" ]] || [[ "${mode}" == "wr" ]]; then
+          if [[ "${mode}" == "w" ]]; then
+            echo "Adding ${accessName} write access rights to container path: ${containerPath}"
+            containerCommand "${containerName}" "chmod ${accessPrefix}+w ${containerPath}"
+          else
+            echo "Adding ${accessName} write access rights to container path: ${containerPath} recursively"
+            containerCommand "${containerName}" "chmod -R ${accessPrefix}+w ${containerPath}"
+          fi
+          accessRights=$(containerCommandQuiet "${containerName}" "stat -L -c \"%a\" ${containerPath}")
+          if [[ "${containerUserName}" == "${accessUser}" ]]; then
+            accessRight="${accessRights:0:1}"
+          else
+            accessRight="${accessRights:1:1}"
+          fi
+          if [[ "${accessRight}" == 2 ]] || [[ "${accessRight}" == 3 ]] || [[ "${accessRight}" == 6 ]] || [[ "${accessRight}" == 7 ]]; then
+            echo "Successfully added ${accessName} write access rights to container path: ${containerPath}" | sed $'s,.*,\e[1;36m&\e[m,'
+          else
+            >&2 echo "Could not add ${accessName} access rights to container path: ${containerPath}"
+            exit 1
+          fi
+        elif [[ "${mode}" == "x" ]] || [[ "${mode}" == "xr" ]]; then
+          if [[ "${mode}" == "x" ]]; then
+            echo "Adding ${accessName} execution access rights to container path: ${containerPath}"
+            containerCommand "${containerName}" "chmod ${accessPrefix}+x ${containerPath}"
+          else
+            echo "Adding ${accessName} execution access rights to container path: ${containerPath} recursively"
+            containerCommand "${containerName}" "chmod -R ${accessPrefix}+x ${containerPath}"
+          fi
+          accessRights=$(containerCommandQuiet "${containerName}" "stat -L -c \"%a\" ${containerPath}")
+          if [[ "${containerUserName}" == "${accessUser}" ]]; then
+            accessRight="${accessRights:0:1}"
+          else
+            accessRight="${accessRights:1:1}"
+          fi
+          if [[ "${accessRight}" == 1 ]] || [[ "${accessRight}" == 3 ]] || [[ "${accessRight}" == 5 ]] || [[ "${accessRight}" == 7 ]]; then
+            echo "Successfully added ${accessName} execution access rights to container path: ${containerPath}" | sed $'s,.*,\e[1;36m&\e[m,'
+          else
+            >&2 echo "Could not add ${accessName} execution access rights to container path: ${containerPath} to: ${accessRights}"
+            exit 1
+          fi
+        elif [[ "${mode}" == "o" ]] || [[ "${mode}" == "or" ]]; then
+          if [[ "${mode}" == "o" ]]; then
+            echo "Changing user of container path: ${containerPath} to: ${accessUser}"
+            containerCommand "${containerName}" "chown ${accessUser} ${containerPath}"
+          else
+            echo "Changing user of container path: ${containerPath} to: ${accessUser} recursively"
+            containerCommand "${containerName}" "chown -R ${accessUser} ${containerPath}"
+          fi
+          containerUserName=$(containerCommandQuiet "${containerName}" "stat -L -c \"%U\" ${containerPath}")
+          if [[ "${containerUserName}" == "${accessUser}" ]]; then
+            echo "Successfully changed owner of container path: ${containerPath} to: ${accessUser}" | sed $'s,.*,\e[1;36m&\e[m,'
+          else
+            >&2 echo "Could not change owner of container path: ${containerPath} to: ${accessUser}"
+            exit 1
+          fi
+        elif [[ -n "${mode}" ]] && [[ "${mode}" != "-" ]]; then
+          >&2 echo "Unknown container path mode: ${mode} to container path: ${containerPath}"
           exit 1
         fi
-      elif [[ "${mode}" == "w" ]] || [[ "${mode}" == "wr" ]]; then
-        if [[ "${mode}" == "w" ]]; then
-          echo "Adding ${accessName} write access rights to container path: ${containerPath}"
-          containerCommand "${containerName}" "chmod ${accessPrefix}+w ${containerPath}"
-        else
-          echo "Adding ${accessName} write access rights to container path: ${containerPath} recursively"
-          containerCommand "${containerName}" "chmod -R ${accessPrefix}+w ${containerPath}"
-        fi
-        accessRights=$(containerCommandQuiet "${containerName}" "stat -L -c \"%a\" ${containerPath}")
-        if [[ "${containerUserName}" == "${accessUser}" ]]; then
-          accessRight="${accessRights:0:1}"
-        else
-          accessRight="${accessRights:1:1}"
-        fi
-        if [[ "${accessRight}" == 2 ]] || [[ "${accessRight}" == 3 ]] || [[ "${accessRight}" == 6 ]] || [[ "${accessRight}" == 7 ]]; then
-          echo "Successfully added ${accessName} write access rights to container path: ${containerPath}" | sed $'s,.*,\e[1;36m&\e[m,'
-        else
-          >&2 echo "Could not add ${accessName} access rights to container path: ${containerPath}"
-          exit 1
-        fi
-      elif [[ "${mode}" == "x" ]] || [[ "${mode}" == "xr" ]]; then
-        if [[ "${mode}" == "x" ]]; then
-          echo "Adding ${accessName} execution access rights to container path: ${containerPath}"
-          containerCommand "${containerName}" "chmod ${accessPrefix}+x ${containerPath}"
-        else
-          echo "Adding ${accessName} execution access rights to container path: ${containerPath} recursively"
-          containerCommand "${containerName}" "chmod -R ${accessPrefix}+x ${containerPath}"
-        fi
-        accessRights=$(containerCommandQuiet "${containerName}" "stat -L -c \"%a\" ${containerPath}")
-        if [[ "${containerUserName}" == "${accessUser}" ]]; then
-          accessRight="${accessRights:0:1}"
-        else
-          accessRight="${accessRights:1:1}"
-        fi
-        if [[ "${accessRight}" == 1 ]] || [[ "${accessRight}" == 3 ]] || [[ "${accessRight}" == 5 ]] || [[ "${accessRight}" == 7 ]]; then
-          echo "Successfully added ${accessName} execution access rights to container path: ${containerPath}" | sed $'s,.*,\e[1;36m&\e[m,'
-        else
-          >&2 echo "Could not add ${accessName} execution access rights to container path: ${containerPath} to: ${accessRights}"
-          exit 1
-        fi
-      elif [[ "${mode}" == "o" ]] || [[ "${mode}" == "or" ]]; then
-        if [[ "${mode}" == "o" ]]; then
-          echo "Changing user of container path: ${containerPath} to: ${accessUser}"
-          containerCommand "${containerName}" "chown ${accessUser} ${containerPath}"
-        else
-          echo "Changing user of container path: ${containerPath} to: ${accessUser} recursively"
-          containerCommand "${containerName}" "chown -R ${accessUser} ${containerPath}"
-        fi
-        containerUserName=$(containerCommandQuiet "${containerName}" "stat -L -c \"%U\" ${containerPath}")
-        if [[ "${containerUserName}" == "${accessUser}" ]]; then
-          echo "Successfully changed owner of container path: ${containerPath} to: ${accessUser}" | sed $'s,.*,\e[1;36m&\e[m,'
-        else
-          >&2 echo "Could not change owner of container path: ${containerPath} to: ${accessUser}"
-          exit 1
-        fi
-      elif [[ -n "${mode}" ]] && [[ "${mode}" != "-" ]]; then
-        >&2 echo "Unknown container path mode: ${mode} to container path: ${containerPath}"
-        exit 1
+      else
+        echo "Container path: ${containerPath} does not exist" | sed $'s,.*,\e[1;33m&\e[m,'
       fi
     else
       >&2 echo "No container path to prepare"
