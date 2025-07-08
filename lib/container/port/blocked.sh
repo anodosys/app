15d13763dd8b8ef170b97cfbdaf0a8b01da5d18e
Andreas Knollmann <a.knollmann@seak.dev>
2025-07-08 10:00:56 +0200

    update pruning

--- lib/container/port/blocked.sh
+++ lib/container/port/blocked.sh
@@ -10,7 +10,10 @@ containerPortBlocked()
   if [[ $(containerExists "${containerName}") == 1 ]] && [[ $(containerRunning "${containerName}") == 0 ]]; then
     ports=( $(containerPortHostList "${containerName}") )
     for port in "${ports[@]}"; do
-      portBlocked=$(bash -c "echo > /dev/tcp/127.0.0.1/${port}" 2>/dev/null && echo 1 || echo 0)
+      readarray -d / -t portParts < <(printf '%s' "${port}")
+      port="${portParts[0]}"
+      protocol="${portParts[1]}"
+      portBlocked=$(bash -c "echo > /dev/${protocol}/127.0.0.1/${port}" 2>/dev/null && echo 1 || echo 0)
       if [[ "${portBlocked}" == 1 ]]; then
         echo 1
         exit 0
