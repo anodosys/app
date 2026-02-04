c41725b1a48411779a0a0146fe026b1738e8f7aa
Andreas Knollmann <a.knollmann@seak.dev>
2026-02-04 10:35:33 +0100

    fix path check

--- lib/container/path.sh
+++ lib/container/path.sh
@@ -8,14 +8,14 @@ containerPath()
   local mode="${4}"
   local accessRights="${5}"
   local missingMode="${6}"
-  local containerGroupId
-  local containerGroupName
-  local containerUserId
-  local containerUserName
-  local result
   local accessPrefix
   local accessName
   local accessRight
+  local containerUserId
+  local containerUserName
+  local containerAccessUser
+  local containerPathGroupNames
+  local containerPathGroupName
 
   if [[ $(containerExists "${containerName}") == 1 ]]; then
     if [[ -n "${containerPath}" ]]; then
@@ -35,52 +35,19 @@ containerPath()
           containerCommand "${containerName}" "[[ -e ${containerPath} ]] && chmod ${accessRights} ${containerPath} || echo \"Path not available\""
         fi
 
-        containerGroupId=$(containerCommandQuiet "${containerName}" "stat -c \"%g\" ${containerPath}")
-        containerGroupName=$(containerCommandQuiet "${containerName}" "getent group ${containerGroupId} | tr ':' ' ' | awk '{print \$1}'")
-        containerGroupName=$(prepareValue "${containerGroupName}")
+        containerPathGroup "${containerName}" "${containerPath}"
+        containerPathUser "${containerName}" "${containerPath}"
 
-        if [[ -z "${containerGroupName}" ]]; then
-          containerGroupName="docker_volume_${containerGroupId}"
-          echo "Creating new group: ${containerGroupName}"
-          containerCommand "${containerName}" "groupadd -g ${containerGroupId} ${containerGroupName}"
-        else
-          echo "No need to create group: ${containerGroupName}"
+        if [[ "${accessUser}" == "local" ]] || [[ "${accessUser}" == "me" ]]; then
+          containerPathLocalUser "${containerName}" "${containerPath}"
         fi
 
         containerUserId=$(containerCommandQuiet "${containerName}" "stat -L -c \"%u\" ${containerPath}")
         containerUserName=$(containerCommandQuiet "${containerName}" "getent passwd ${containerUserId} | cat" | tr ':' ' ' | awk '{print $1}')
         containerUserName=$(prepareValue "${containerUserName}")
 
-        if [[ -z "${containerUserName}" ]]; then
-          containerUserName="docker_volume_${containerUserId}"
-          echo "Creating new user: ${containerUserName}"
-          containerCommand "${containerName}" "useradd -m -u ${containerUserId} -g ${containerGroupId} ${containerUserName}"
-        else
-          echo "No need to create user: ${containerUserName}"
-        fi
-
-        if [[ "${accessUser}" == "local" ]] || [[ "${accessUser}" == "me" ]]; then
-          userId="${UID}"
-          accessUser=$(containerCommandQuiet "${containerName}" "getent passwd ${userId} | tr ':' ' ' | awk '{print \$1}'")
-          accessUser=$(prepareValue "${accessUser}")
-
-          if [[ -z "${accessUser}" ]]; then
-            accessUser="docker_volume_${userId}"
-            echo "Creating new user: ${accessUser}"
-            containerCommand "${containerName}" "useradd -m -u ${userId} -g ${containerGroupId} ${accessUser}"
-          fi
-        fi
-
         if [[ "${containerUserName}" != "${accessUser}" ]]; then
-          result=$(containerCommandQuiet "${containerName}" "id -nG ${accessUser} | grep -w ${containerGroupName} | wc -l")
-          result=$(prepareValue "${result}")
-
-          if [[ "${result}" == 0 ]]; then
-            echo "Adding user: ${accessUser} to group: ${containerGroupName}"
-            containerCommand "${containerName}" "usermod -a -G ${containerGroupName} ${accessUser}"
-          else
-            echo "No need to add user: ${accessUser} to group: ${containerGroupName}"
-          fi
+          containerPathUserGroup "${containerName}" "${containerPath}" "${accessUser}"
         else
           echo "No different user for container path: ${containerPath}"
         fi
@@ -100,6 +67,17 @@ containerPath()
           else
             echo "Adding ${accessName} read access rights to container path: ${containerPath} recursively"
             containerCommand "${containerName}" "chmod -R ${accessPrefix}+r ${containerPath}"
+            if [[ "${accessUser}" == "local" ]] || [[ "${accessUser}" == "me" ]]; then
+              userId="${UID}"
+              containerAccessUser=$(containerCommandQuiet "${containerName}" "getent passwd ${userId} | tr ':' ' ' | awk '{print \$1}'")
+              containerAccessUser=$(prepareValue "${containerAccessUser}")
+            else
+              containerAccessUser="${accessUser}"
+            fi
+            containerPathGroupNames=( $(containerCommandQuiet "${containerName}" "find \"${containerPath}\" ! -user \"${containerAccessUser}\" -exec stat -c \"%G\" {} \; | sort -u | grep -vw 0 | cat") )
+            for containerPathGroupName in "${containerPathGroupNames[@]}"; do
+              containerUserGroup "${containerName}" "${accessUser}" "${containerPathGroupName}"
+            done
           fi
           accessRights=$(containerCommandQuiet "${containerName}" "stat -L -c \"%a\" ${containerPath}")
           if [[ "${containerUserName}" == "${accessUser}" ]]; then
@@ -120,6 +98,17 @@ containerPath()
           else
             echo "Adding ${accessName} write access rights to container path: ${containerPath} recursively"
             containerCommand "${containerName}" "chmod -R ${accessPrefix}+w ${containerPath}"
+            if [[ "${accessUser}" == "local" ]] || [[ "${accessUser}" == "me" ]]; then
+              userId="${UID}"
+              containerAccessUser=$(containerCommandQuiet "${containerName}" "getent passwd ${userId} | tr ':' ' ' | awk '{print \$1}'")
+              containerAccessUser=$(prepareValue "${containerAccessUser}")
+            else
+              containerAccessUser="${accessUser}"
+            fi
+            containerPathGroupNames=( $(containerCommandQuiet "${containerName}" "find \"${containerPath}\" ! -user \"${containerAccessUser}\" -exec stat -c \"%G\" {} \; | sort -u | grep -vw 0 | cat") )
+            for containerPathGroupName in "${containerPathGroupNames[@]}"; do
+              containerUserGroup "${containerName}" "${containerAccessUser}" "${containerPathGroupName}"
+            done
           fi
           accessRights=$(containerCommandQuiet "${containerName}" "stat -L -c \"%a\" ${containerPath}")
           if [[ "${containerUserName}" == "${accessUser}" ]]; then
@@ -192,3 +181,137 @@ containerPath()
 
 # shellcheck disable=SC2034
 typeset -fx containerPath
+
+containerPathGroup()
+{
+  local containerName="${1}"
+  local containerPath="${2}"
+  local containerGroupId
+  local containerGroupName
+
+  if [[ $(containerExists "${containerName}") == 1 ]]; then
+    if [[ -n "${containerPath}" ]]; then
+      if [[ $(containerCommandQuiet "${containerName}" "[[ -e ${containerPath} ]] && echo \"true\" || echo \"false\"") == "true" ]]; then
+        containerGroupId=$(containerCommandQuiet "${containerName}" "stat -c \"%g\" ${containerPath}")
+        containerGroupName=$(containerCommandQuiet "${containerName}" "getent group ${containerGroupId} | tr ':' ' ' | awk '{print \$1}'")
+        containerGroupName=$(prepareValue "${containerGroupName}")
+
+        if [[ -z "${containerGroupName}" ]]; then
+          containerGroupName="docker_volume_${containerGroupId}"
+          echo "Creating new group: ${containerGroupName}"
+          containerCommand "${containerName}" "groupadd -g ${containerGroupId} ${containerGroupName}"
+        else
+          echo "No need to create group: ${containerGroupName}"
+        fi
+      fi
+    fi
+  fi
+}
+
+# shellcheck disable=SC2034
+typeset -fx containerPathGroup
+
+containerPathUser()
+{
+  local containerName="${1}"
+  local containerPath="${2}"
+  local containerUserId
+  local containerUserName
+  local containerGroupId
+
+  if [[ $(containerExists "${containerName}") == 1 ]]; then
+    if [[ -n "${containerPath}" ]]; then
+      if [[ $(containerCommandQuiet "${containerName}" "[[ -e ${containerPath} ]] && echo \"true\" || echo \"false\"") == "true" ]]; then
+        containerUserId=$(containerCommandQuiet "${containerName}" "stat -L -c \"%u\" ${containerPath}")
+        containerUserName=$(containerCommandQuiet "${containerName}" "getent passwd ${containerUserId} | cat" | tr ':' ' ' | awk '{print $1}')
+        containerUserName=$(prepareValue "${containerUserName}")
+
+        if [[ -z "${containerUserName}" ]]; then
+          containerGroupId=$(containerCommandQuiet "${containerName}" "stat -c \"%g\" ${containerPath}")
+
+          containerUserName="docker_volume_${containerUserId}"
+          echo "Creating new user: ${containerUserName}"
+          containerCommand "${containerName}" "useradd -m -u ${containerUserId} -g ${containerGroupId} ${containerUserName}"
+        else
+          echo "No need to create user: ${containerUserName}"
+        fi
+      fi
+    fi
+  fi
+}
+
+# shellcheck disable=SC2034
+typeset -fx containerPathUser
+
+containerPathUserGroup()
+{
+  local containerName="${1}"
+  local containerPath="${2}"
+  local accessUser="${3}"
+  local containerGroupId
+  local containerGroupName
+  local userId
+  local result
+
+  containerGroupId=$(containerCommandQuiet "${containerName}" "stat -c \"%g\" ${containerPath}")
+  containerGroupName=$(containerCommandQuiet "${containerName}" "getent group ${containerGroupId} | tr ':' ' ' | awk '{print \$1}'")
+  containerGroupName=$(prepareValue "${containerGroupName}")
+
+  containerUserGroup "${containerName}" "${accessUser}" "${containerGroupName}"
+}
+
+# shellcheck disable=SC2034
+typeset -fx containerPathUserGroup
+
+containerUserGroup()
+{
+  local containerName="${1}"
+  local accessUser="${2}"
+  local containerGroupName="${3}"
+  local userId
+  local result
+
+  if [[ "${accessUser}" == "local" ]] || [[ "${accessUser}" == "me" ]]; then
+    userId="${UID}"
+    accessUser=$(containerCommandQuiet "${containerName}" "getent passwd ${userId} | tr ':' ' ' | awk '{print \$1}'")
+    accessUser=$(prepareValue "${accessUser}")
+  fi
+
+  result=$(containerCommandQuiet "${containerName}" "id -nG ${accessUser} | grep -w ${containerGroupName} | wc -l")
+  result=$(prepareValue "${result}")
+
+  if [[ "${result}" == 0 ]]; then
+    echo "Adding user: ${accessUser} to group: ${containerGroupName}"
+    containerCommand "${containerName}" "usermod -a -G ${containerGroupName} ${accessUser}"
+  else
+    echo "No need to add user: ${accessUser} to group: ${containerGroupName}"
+  fi
+}
+
+# shellcheck disable=SC2034
+typeset -fx containerUserGroup
+
+containerPathLocalUser()
+{
+  local containerName="${1}"
+  local containerPath="${2}"
+  local userId
+  local accessUser
+  local containerGroupId
+
+  userId="${UID}"
+  accessUser=$(containerCommandQuiet "${containerName}" "getent passwd ${userId} | tr ':' ' ' | awk '{print \$1}'")
+  accessUser=$(prepareValue "${accessUser}")
+
+  if [[ -z "${accessUser}" ]]; then
+    accessUser="docker_volume_${userId}"
+    containerGroupId=$(containerCommandQuiet "${containerName}" "stat -c \"%g\" ${containerPath}")
+    echo "Creating new user: ${accessUser} with group id: ${containerGroupId}"
+    containerCommand "${containerName}" "useradd -m -u ${userId} -g ${containerGroupId} ${accessUser}"
+  else
+    echo "No need to create user: ${accessUser}"
+  fi
+}
+
+# shellcheck disable=SC2034
+typeset -fx containerPathLocalUser
