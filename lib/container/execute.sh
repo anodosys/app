4816e4cd5c8233cdcf136ac991bf4922c7cff2cf
Andreas Knollmann <a.knollmann@seak.dev>
2026-02-06 16:54:35 +0100

    added check for user name

--- lib/container/execute.sh
+++ lib/container/execute.sh
@@ -37,6 +37,10 @@ containerExecuteUser()
   local localFileName="${1}"
   shift
   local parameters=("$@")
+  local userId
+  local userHome
+  local groupId
+  local groupName
   local remoteFileName
 
   if [[ -z "${userName}" ]]; then
@@ -44,11 +48,34 @@ containerExecuteUser()
     exit 1
   fi
 
+  if [[ "${userName}" == "local" ]] || [[ "${userName}" == "me" ]]; then
+    userId="${UID}"
+    userName=$(containerCommandQuiet "${containerName}" "getent passwd ${userId} | tr ':' ' ' | awk '{print \$1}'")
+    if [[ -z "${userName}" ]]; then
+      userName="${USER}"
+      userHome=$(getent passwd "${userName}" | tr ':' ' ' | awk '{print $6}')
+      groupId=$(stat -c '%g' "${userHome}")
+      groupName=$(containerCommandQuiet "${containerName}" "getent group ${groupId} | tr ':' ' ' | awk '{print \$1}'")
+      if [[ -z "${groupName}" ]]; then
+        groupName="docker_volume_${groupId}"
+        echo "Creating new group: ${groupName}"
+        containerCommand "${containerName}" "groupadd -g ${groupId} ${groupName}"
+      else
+        echo "No need to create group: ${groupName}"
+      fi
+      userName="docker_volume_${userId}"
+      echo "Creating new user: ${userName}"
+      containerCommand "${containerName}" "useradd -m -u ${userId} -g ${groupId} ${userName}"
+    else
+      userName=$(prepareValue "${userName}")
+    fi
+  fi
+
   if [[ -f "${localFileName}" ]]; then
     if [[ $(containerRunning "${containerName}") == 1 ]]; then
       containerCopy "${containerName}" "${localFileName}"
       remoteFileName=$(basename "${localFileName}")
-      echo "Executing script in container at: /${remoteFileName} ${parameters[*]}"
+      echo "Executing script in container at: /${remoteFileName} with user: ${userName} ${parameters[*]}"
       docker exec --user "${userName}" "${containerName}" "/${remoteFileName}" "${parameters[@]}"
     else
       >&2 echo "Container not running: ${containerName}"
